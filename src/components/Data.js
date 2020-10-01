4ad87b8dea06e32c305a478f99fcf173817a4d2d
SAEBOMPAK <saebompak@gmail.com>
2020-10-01 13:21:59 +0900

    project page update

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,11 +1,20 @@
 export let DATA = [
     {
       id:1,
-      imgtitle: 'ezrealty',
-    //   src: require('../../../images/project01.jpg'),
-      alt:'ezrealty',
-      name:'ezrealty',
+      imgtitle: 'school',
+      src: require('../images/prj01.png'),
+      alt:'school',
+      name:'This is a school project',
       title:'PHP, HTML, CSS, Responsive Web',
-      href:"https://www.ezrealty.com",
+      href:"https://www.school.com",
+    },
+    {
+      id:2,
+      imgtitle: 'school',
+      src: require('../images/prj02.png'),
+      alt:'school',
+      name:'This is a school project',
+      title:'React, Node',
+      href:"https://www.school.com",
     }
 ]
--- src/components/List.js
+++ src/components/List.js
@@ -3,21 +3,15 @@ import React, { useState, useEffect, Typography } from 'react';
 //import Linkedin from '../../../images/linkedin.png';
 
 const List = (props) => (
-    <div className="items0">
-    <div className="tPic">
-      <img title={props.imgtitle} src={props.src} alt={props.alt} />
-    </div>
-    <div className="itemTitle">{props.name}</div>
-    <div className="itemDetail">{props.title}</div>
-    <div className="link">
-      <a href={props.ahref} target="_blank">
-          <img 
-            style={{width:'28px'}} 
-            title="linkedin" 
-            // src={Linkedin} 
-            alt="ez" />
-      </a>
-    </div>
+    <div className="items">
+      <div className="tPic">
+        <img title={props.imgtitle} src={props.src} alt={props.alt} />
+      </div>
+      <ul>
+        <li>{props.name}</li>
+        <li>{props.title}</li>
+        <li><a href={props.ahref} target="_blank">{props.ahref}</a></li>
+      </ul>
   </div>
 );
 
