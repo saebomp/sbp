c305ec9084dacc9f1ae53c538afe327585f6ac32
SAEBOMPAK <saebompak@gmail.com>
2020-11-15 16:08:12 +0900

    updated about page

--- src/components/About.js
+++ src/components/About.js
@@ -8,23 +8,37 @@ class About extends Component {
         <div class="wrapper">
           <h1>About</h1>
           <div className="container">
-              <ul>
-                <li><h4>Skill</h4></li>
-                <li>매니</li>
-              </ul>
-              <ul>
-                <li><h4>Education</h4></li>
-                <li>랑가라</li>
-              </ul>
-              <ul>
-                <li><h4>Work Experience</h4></li>
-                <li>다수</li>
-              </ul>
-              <ul>
-                <li><h4>Work Experience</h4></li>
-                <li>다수</li>
-              </ul>
-            </div>
+            <ul>
+              <li><h4>Profile</h4></li>
+              <li>Self-directed, motivated front-end developer who loves building user-friendly websites and web apps with best practices. Eagerness to improve developing skills, Focusing on clean and efficient code. </li>
+            </ul>
+            <ul>
+              <li><h4>Skills</h4></li>
+              <li>React / React Native / JavaScript / HTML5 / CSS / SASS / Github</li>
+            </ul>
+            <ul>
+              <li><h4>Work Experience</h4></li>
+              <li>
+                Littlefox 2017~2018 <br />
+                - Updated code to improve site performance.<br />
+                - Utilized JavaScript, HTML 5 and CSS<br />
+                - Worked closely with design marketing teams within an agile team environment.<br />
+                <br />
+                Whatever Search 2015~2016<br />
+                - Built several responsive websites with HTML, CSS, JQuery and Bootstrap<br />
+                - Worked across multiple devices and browsers<br />
+                <br />
+                Gensol Soft  2014<br />
+                - Built National assembly minutes’ website with Web Accessibility guidelines,  HTML and CSS (http://likms.assembly.go.kr/record/)
+                - Built Asian Airlines’s policy manual system front-end with HTML and CSS
+                </li>
+            </ul>
+            <ul>
+              <li><h4>Education</h4></li>
+              <li>Web and Mobile App Design and Development, Langara College 
+</li>
+            </ul>
+          </div>
         </div>
       </div>
       );
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -9,7 +9,7 @@ class Homepage extends Component {
           <h1>Home</h1>
           <div className="container">
             <p className="about">
-            Welcome to my portfolio.<br />
+            {/* Welcome to my portfolio.<br /> */}
             I’m a front-end web developer based in Burnaby.<br />
             While my focus is responsive websites with React, JavaScript, HTML and CSS, I also do love designing. I have some work experience as an web designer which menas that I am familiar with Adobe Photoshop, Illustrator and Premiere. 
             I am always learning and passionate about my craft. 
