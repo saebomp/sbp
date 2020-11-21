61e72bd5c5c7031d65388eb718d018ce080fa892
SAEBOMPAK <saebompak@gmail.com>
2020-11-22 08:49:58 +0900

    added projects

--- src/components/Data.js
+++ src/components/Data.js
@@ -18,25 +18,29 @@ export let DATA = [
     },
     {
       id:2,
-      imgtitle: 'school',
-      src: require('../images/prj02.png'),
-      alt:'school',
-      name:'This is a school project2',
-      skill:["React", "Node"],
+      imgtitle: 'movie app',
+      src: require('../images/p02.png'),
+      alt:'movie app',
+      name:'Movie App',
+      skill:["React", "Responsive Web"],
       href:"https://www.school.com",
-      desc1:"This is my project I did it all by myself",
-      desc2:["옹", "무엇"]
+      // desc1:"This is my project I did it all by myself",
+      desc2:[
+        "- Requested to movie API and displayed movie and tv lists.",
+        "- Upon selecting another selection from that dropdown box, the list of results should update to reflect the updated response from the new query.",
+        "- Utilized React and Material UI"
+      ]
     },
     {
       id:3,
-      imgtitle: 'school',
-      src: require('../images/prj01.png'),
-      alt:'school',
-      name:'This is a school project3',
-      skill:["React", "Node"],
-      href:"https://www.school.com",
-      desc1:"This is my project I did it all by myself",
-      desc2:["옹", "무엇"]
+      imgtitle: 'National Assembly Minutes',
+      src: require('../images/p03.png'),
+      alt:'National Assembly Minutes',
+      name:'National Assembly Minutes',
+      skill:["HTML", "CSS", "Web Accessibility"],
+      href:"http://likms.assembly.go.kr/record/",
+      desc1:"This was built from 2016 Aug to 2016 Dec. I implemented web accessibility with Web Content Accessibility Guidelines (WCAG). I was able to gain a greater comprehension of cross-browsing",
+      desc2:[]
     },
     {
       id:4,
