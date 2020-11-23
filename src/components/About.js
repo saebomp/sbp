569bf3b321248fa943198395823efe054f786710
SAEBOMPAK <saebompak@gmail.com>
2020-11-23 09:28:57 +0900

    updated contact

--- src/components/About.js
+++ src/components/About.js
@@ -8,41 +8,48 @@ class About extends Component {
         <div class="wrapper">
           <h1>About</h1>
           <div className="container">
-            <ul className="about">
-              <li><h4>Profile</h4></li>
-              <li>Self-directed, motivated front-end developer who loves building user-friendly websites and web apps with best practices. Eagerness to improve developing skills, Focusing on clean and efficient code.</li>
-              <li><h4>Skills</h4></li>
-              <li>React / React Native / JavaScript / HTML5 / CSS / SASS / Github</li>
-              <li><h4>Projects</h4></li>
-              <li>
-                <u>Kovan</u><br /> 
-                Kovan is a solution to manage all kinds of small groups effectively. This was my capstone project for WMDD Langara college. It was built in 3 months by 5 developers.<br /> <br /> 
+            <div className="about">
+              <h4>Profile</h4>
+              <p>Self-directed, motivated front-end developer who loves building user-friendly websites and web apps with best practices. Eagerness to improve developing skills, Focusing on clean and efficient code.</p>
 
-                What I did:<br /> 
-                - Built To-do list and polling page with React Native<br /> 
-                - Implemented interactive graph chart with React Native svg<br /> 
-                - Applied consistent styling for Expenses page<br /> 
-                - Built the promotional website of Kovan with React <a href="https://kovanapp.com/" target="_blank">(https://kovanapp.com/)</a>
-              </li>
-              <li><h4>Work Experience</h4></li>
-              <li>
-                Littlefox <span>(2017~2018)</span> <br />
-                - Updated code to improve site performance.<br />
-                - Utilized JavaScript, HTML 5 and CSS<br />
-                - Worked closely with design marketing teams within an agile team environment.<br />
-                <br />
-                Whatever Search <span>(2015~2016)</span><br />
-                - Built several responsive websites with HTML, CSS, JQuery and Bootstrap<br />
-                - Worked across multiple devices and browsers<br />
-                <br />
-                Gensol Soft <span>(2014)</span><br />
-                - Built National assembly minutes’ website with Web Accessibility guidelines,  HTML and CSS <br />
-                - Built Asian Airlines’s policy manual system front-end with HTML and CSS
-                </li>
-              <li><h4>Education</h4></li>
-              <li>Web and Mobile App Design and Development, Langara College 
-</li>
-            </ul>
+              <h4>Skills</h4>
+              <p>React / React Native / JavaScript / GraphQL / HTML5 / CSS / SASS / Github</p>
+
+              <h4>Work Experience</h4>
+              <h5>Littlefox <span>/ UI developer (Jan 2017 - Aug 2018)</span></h5>
+              <ul>
+                <li>Updated code to improve site performance with JavaScript, HTML 5 and CSS</li>
+                <li>Worked across multiple devices and browsers</li>
+                <li>Worked closely with design and marketing teams within an agile team environment.</li>
+              </ul>
+
+              <h6>RESULT</h6>
+              <p>This work environment allowed me to develop my communication and interpersonal skills. An enriched understanding of multiple devices was acquired.</p>
+
+              <h5>Whatever Search <span>/ UI developer (Oct 2015 - Dec 2016)</span></h5>
+              <ul>
+                <li>Built responsive websites with HTML, CSS, JQuery and Bootstrap</li>
+                <li>Fixed bugs which were caused by old web browsers.</li>
+                <li>Transitioned static websites to responsiveness</li>
+              </ul>
+              
+              <h6>RESULT</h6>
+              <p>I was able to gain a greater comprehension of responsive web and cross-browsing. </p>
+                
+              <h5>Gensol Soft <span>/ UI developer (Jun 2014 - Mar 2015)</span></h5>
+              <ul>
+                <li>Built National assembly minutes’ website with Web Accessibility guidelines,  HTML and CSS</li>
+                <li>Built Asian Airlines’s policy manual system front-end with HTML and CSS</li>
+              </ul>
+
+              <h6>RESULT</h6>
+              <p>
+              I learned how to make a website accessible and became familiar with UX design while transitioning to a user-friendly website. I also improved my time management skill from working with clients.</p>
+
+              <h4>Education</h4>
+              <p>Web and Mobile App Design and Development, Langara College 
+</p>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,15 +5,25 @@ class Contact extends Component {
     return (
       <div className="main">
         <div class="wrapper">
-          <h1>Contact</h1>
-          <div className="container">
-             Phone
-             Email
-            </div>
+            <h1>Contact</h1>
+            <div className="container">
+              <div className="contact">
+                <ul>
+                  <li className="bg bg1">saebompak@gmail.com</li>
+                  <li className="bg bg2">(236) 986-5481</li>
+                  <li className="bg bg3">4670 Assembly way, Burnaby, BC</li>
+                </ul>
+                <ul>
+                  <li className="bg bg4"><a href="https://www.github.com/saebomp" target="_blank">github.com/saebomp</a></li>
+                  <li className="bg bg5"><a href="https://www.linkedin.com/in/saebompak" target="_blank">linkedin.com/in/saebompak</a></li>
+                </ul>
+              </div>
           </div>
+        </div>
       </div>
       );
   }
 }
   
-  export default Contact;
+export default Contact;
+
