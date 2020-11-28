0142160934464e3b4dc806bc94eb569b2b133b31
SAEBOMPAK <saebompak@gmail.com>
2020-11-28 09:26:39 +0900

    changed link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -49,7 +49,7 @@ export default function Nav() {
                 </NavLink>
             </li> */}
             <li>
-                <NavLink exact to="/" className="menu_item" activeClassName="active">
+                <NavLink exact={true} to="/" className="menu_item" activeClassName="active">
                     <span class="menu_item_name">About</span>
                 </NavLink>
             </li>
