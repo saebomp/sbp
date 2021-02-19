00d4d68f902c3f20e4e054d904d91b0bd31de885
SAEBOMPAK <saebompak@gmail.com>
2021-02-19 14:37:23 +0900

    switched order

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,6 +1,53 @@
 export let DATA = [
+    
     {
       id:1,
+      imgtitle: 'Smart Movie and TV Finder',
+      src: require('../images/p02.png'),
+      alt:'Smart Movie and TV Finder',
+      name:'Smart Movie and TV Finder',
+      skill:["React", "Responsive Web", "CSS", "Material-UI"],
+      period:'October 2020',
+      href:"https://saebomp.github.io/movie_app",
+      // desc1:"",
+      desc2:[
+        "Smart Movie and TV Finder provides movie and tv shows lists upon user's request.",
+        "Upon selecting another selection from that dropdown box, the list of results refresh to reflect the updated response from the new query.",
+        "Utilized React and Material UI."
+      ]
+    },
+    {
+      id:2,
+      imgtitle: 'News app',
+      src: require('../images/p04.png'),
+      alt:'News app',
+      name:'News app',
+      skill:["React", "Responsive Web", "GraphQl", "CSS", "Ant Design"],
+      href:"https://github.com/saebomp/newsql",
+      // desc1:"This is my projectn",
+      desc2:[
+        "Fetched data from a REST Endpoint with Apollo using GraphQL query language.", 
+        "Displayed results from the remote API endpoint.",
+        "Fetched new data when the tab is switched."
+      ]
+    },
+    {
+      id:3,
+      imgtitle: 'Expense Tracker',
+      src: require('../images/p03.png'),
+      alt:'Expense Tracker',
+      name:'Expense Tracker',
+      skill:["React", "React Hooks", "D3.js", "Tailwind"],
+      period:'Aug 2016 - Dec 2016',
+      href:"https://saebomp.github.io/expenseTracker/",
+      // desc1:"",
+      desc2:[
+        "Learned how to make a website accessible and became familiar with UX design while transitioning to a user-friendly website.",
+        "Improved time management skills when working with clients."
+      ]
+    },
+    {
+      id:4,
       imgtitle: 'kovan',
       src: require('../images/p01.png'),
       alt:'kovan',
@@ -17,51 +64,20 @@ export let DATA = [
         "Built the promotional website of Kovan with React." 
       ]
     },
-    {
-      id:2,
-      imgtitle: 'Smart Movie and TV Finder',
-      src: require('../images/p02.png'),
-      alt:'Smart Movie and TV Finder',
-      name:'Smart Movie and TV Finder',
-      skill:["React", "Responsive Web", "CSS", "Material-UI"],
-      period:'October 2020',
-      href:"https://saebomp.github.io/movie_app",
-      // desc1:"",
-      desc2:[
-        "Smart Movie and TV Finder provides movie and tv shows lists upon user's request.",
-        "Upon selecting another selection from that dropdown box, the list of results refresh to reflect the updated response from the new query.",
-        "Utilized React and Material UI."
-      ]
-    },
-    {
-      id:3,
-      imgtitle: 'News app',
-      src: require('../images/p04.png'),
-      alt:'News app',
-      name:'News app',
-      skill:["React", "Responsive Web", "GraphQl", "CSS", "Ant Design"],
-      href:"https://github.com/saebomp/newsql",
-      // desc1:"This is my projectn",
-      desc2:[
-        "Fetched data from a REST Endpoint with Apollo using GraphQL query language.", 
-        "Displayed results from the remote API endpoint.",
-        "Fetched new data when the tab is switched."
-      ]
-    },
-    {
-      id:4,
-      imgtitle: 'National Assembly Minutes',
-      src: require('../images/p03.png'),
-      alt:'National Assembly Minutes',
-      name:'National Assembly Minutes',
-      skill:["HTML", "CSS", "Web Accessibility"],
-      period:'Aug 2016 - Dec 2016',
-      href:"http://likms.assembly.go.kr/record/",
-      // desc1:"",
-      desc2:[
-        "Learned how to make a website accessible and became familiar with UX design while transitioning to a user-friendly website.",
-        "Improved time management skills when working with clients."
-      ]
-    },
+    // {
+    //   id:3,
+    //   imgtitle: 'National Assembly Minutes',
+    //   src: require('../images/p03.png'),
+    //   alt:'National Assembly Minutes',
+    //   name:'National Assembly Minutes',
+    //   skill:["HTML", "CSS", "Web Accessibility"],
+    //   period:'Aug 2016 - Dec 2016',
+    //   href:"http://likms.assembly.go.kr/record/",
+    //   // desc1:"",
+    //   desc2:[
+    //     "Learned how to make a website accessible and became familiar with UX design while transitioning to a user-friendly website.",
+    //     "Improved time management skills when working with clients."
+    //   ]
+    // },
     
 ]
