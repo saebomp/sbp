8425ca31a0d9c3dc7b4b1fc15ea5b971f2b5bdcd
SAEBOMPAK <saebompak@gmail.com>
2021-07-08 08:18:03 +0900

    update resume

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -26,7 +26,7 @@ class Intro extends Component {
       <div className="intro" id="Intro">
         <div className="center">
           <div className="text">
-            <h1>Hello, I’m Sae<br />Front-end developer</h1>
+            <h1>Hello, I’m Anne<br />Front-end developer</h1>
             <p>A self-directed, motivated front-end developer who loves building user-friendly websites and web apps with best practices. 
 Eager to improve development skills with a focus on clean and efficient code.</p>
             <div className="download">
