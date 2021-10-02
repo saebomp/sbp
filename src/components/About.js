6c6514202e6dad853b5c2c0f25fe73935e90cc60
SAEBOMPAK <saebompak@gmail.com>
2021-10-02 12:12:07 +0900

    change resume

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ class About extends Component {
         <div className="container">
           <div className="about">
             <h4>Work Experience</h4>
-            <h5>Littlefox <span>(Seoul, Korea) / UI Designer, UI Developer <span class="hidden">(2017 - 2018)</span></span></h5>
+            <h5>Littlefox <span>(Seoul, Korea) / UI Designer, UI Developer <span class="">(2017 - 2018)</span></span></h5>
             <ul>
               <li>Implemented pixel perfect HTML for a design across different web browsers and platforms.</li>
               <li>Translated designs and wireframes into high quality code, complying with best practices.</li>
@@ -17,7 +17,7 @@ class About extends Component {
             <h6 className="hidden">RESULT</h6>
             <p className="hidden">This work environment allowed the development of communication and interpersonal skills. An enriched understanding of multiple devices was acquired.</p>
 
-            <h5>Whatever Search <span>(Seoul, Korea) / UI Developer <span class="hidden">(2015 - 2016)</span></span></h5>
+            <h5>Whatever Search <span>(Seoul, Korea) / UI Developer <span class="">(2015 - 2016)</span></span></h5>
             <ul>
               <li>Developed and maintained responsive web applications leveraging front end development using HTML, CSS and JavaScript.</li>
               <li>Implemented user interfaces for multiple devices according to design.</li>
@@ -27,7 +27,7 @@ class About extends Component {
             <h6 className="hidden">RESULT</h6>
             <p className="hidden">Able to gain a greater comprehension of responsive web and cross-browsing. </p>
               
-            <h5>Gensol Soft <span>(Seoul, Korea) / UI Developer <span class="hidden">(2014 - 2015)</span></span></h5>
+            <h5>Gensol Soft <span>(Seoul, Korea) / UI Developer <span class="">(2014 - 2015)</span></span></h5>
             <ul>
               <li>Worked on a variety of clients and project types.</li>
               <li>Collaborated with the design team to develop and optimize for a consistent user experience.</li>
@@ -38,7 +38,7 @@ class About extends Component {
             Learned how to make a website accessible and became familiar with UX design while transitioning to a user-friendly website. Translated designs and wireframes into responsive websites.</p>
 
             <h4>Education</h4>
-            <p>Web and Mobile App Design and Development, Langara College <span>(Vancouver, BC) <span class="hidden">/ 2019 - 2020</span></span></p>
+            <p>Web and Mobile App Design and Development, Langara College <span>(Vancouver, BC) <span class="">/ 2019 - 2020</span></span></p>
             <ul>
               <li>Earned Dean's Honor for good academic standing.</li>
             </ul>
